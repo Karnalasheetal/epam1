d21bbaf1b4a5349a62b3d82c5d5f75920440eaae
Karnalasheetal <60389463+Karnalasheetal@users.noreply.github.com>
2020-07-25 09:02:03 -0700

    Add files via upload

---
+++ calci/src/Sheetal/Calculator.java
@@ -0,0 +1,5 @@
+package Sheetal;
+
+public class Calculator {
+
+}
---
+++ epam1/src/Chocolates/Cake.java
@@ -0,0 +1,9 @@
+package Chocolates;
+
+public class Cake extends Sweets{
+	Chocolate(String name,int cost,float weight,int Sweetcontent)
+	{
+		super(name,cost,weight,Sweetcontent);
+	}
+
+}
